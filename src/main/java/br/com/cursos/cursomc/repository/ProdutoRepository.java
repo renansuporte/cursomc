78c1c8f210e5654bed896f02021422b1bd87ac46
Renan Santos <renansuporte@bol.com.br>
2019-08-08 21:36:59 -0300

    Associando categoria com produto

---
+++ src/main/java/br/com/cursos/cursomc/repository/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package br.com.cursos.cursomc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.cursos.cursomc.domain.Produto;
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}
